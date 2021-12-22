fa90494917d575c242e24f026b8e47095116f160
87878787878799 <savelev.vitaliy2003@mail.ru>
2021-12-23 01:23:00 +0300

    final 3000

--- task/main.py
+++ task/main.py
@@ -1,37 +1,30 @@
 import time
 from datetime import timedelta
-max_degree = int(input())
-coefs = [int(input()) for i in range(max_degree + 1)]
-
-
-start_time = time.monotonic()
-
 
 
 def main():
+    max_degree = int(input())
+    coefs = [int(input()) for i in range(max_degree + 1)]
+
+    start_time = time.monotonic()
     print("------start------")
     final_string = ""
     count = 0
     for coef in coefs:
-        if coef > 0:
-            if final_string == "":
-                if coef != 1:
-                    final_string += str(coef) + degree_counter(max_degree, count, True)
-                else:
-                    final_string += degree_counter(max_degree, count, False)
-            else:
-                if coef != 1:
-                    final_string += " + " + str(coef) + degree_counter(max_degree, count, True)
-                else:
-                    if max_degree - count == 0:
-                        final_string += " + " + str(coef) + degree_counter(max_degree, count, False)
-                    else:
-                        final_string += " + " + degree_counter(max_degree, count, False)
-        elif coef < 0:
-            if coef != -1:
-                final_string += " - " + str(abs(coef)) + degree_counter(max_degree, count, True)
-            elif (max_degree - count) != 0:
-                final_string += " - " + degree_counter(max_degree, count, False)
+        if coef != 0:
+            coef_module = abs(coef)
+            if coef < 0:
+                final_string += " - "
+            elif final_string != "":
+                final_string += " + "
+            if coef_module != 1 or (max_degree - count) == 0:
+                final_string += str(coef_module)
+            if(max_degree - count) >= 1:
+                if coef_module != 1:
+                    final_string += " * "
+                final_string += "x"
+                if (max_degree - count) > 1:
+                    final_string += "^" + str(max_degree - count)
         count += 1
 
     if final_string == "":
@@ -39,24 +32,9 @@ def main():
     else:
         print(final_string)
 
-
-def degree_counter(degree, cnt, condition: bool) -> str:
-    if degree - cnt == 0:
-        return ""
-    if condition:
-        if degree - cnt == 1:
-            return " * x"
-        else:
-            return " * x^" + str(degree - cnt)
-    else:
-        if degree - cnt == 1:
-            return "x"
-        else:
-            return "x^" + str(degree - cnt)
+    end_time = time.monotonic()
+    print(timedelta(seconds=end_time - start_time))
 
 
 if __name__ == "__main__":
     main()
-
-end_time = time.monotonic()
-print(timedelta(seconds=end_time - start_time))
